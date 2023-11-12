cea9b2c266df8fe4acabdb81bedd54c181f05f21
Rifat Biswas <rifatbiswas2@gmail.com>
2023-11-12 00:15:26 -0800

    [Done]review & massege model done by Rifat

---
+++ Backend/src/models/messageModel.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose")
+
+const messageSchema=mongoose.Schema({
+    
+    ConversationId: {
+        type: String,
+        required: true
+      },
+      UserId: {
+        type: String,
+        required: true
+      },
+      Description: {
+        type: String,
+        required: true
+      }
+
+},{timestamps:true,versionKey:false})
+
+const MessageModel=mongoose.model("orders",messageSchema)
+module.exports=MessageModel;
