6616242f13dbed643b9ab07dc6a530ffeef6779e
Sujon <57975469+SujonHiro@users.noreply.github.com>
2023-11-13 16:29:38 +0600

    [Progressing]-OrderController and orderServices

---
+++ Backend/src/controllers/orderController.js
@@ -0,0 +1,18 @@
+const orderServices=require("../services/orderServices")
+const getOrders=async (req,res)=>{
+    try{
+        const userId=req.userId;
+        const isSeller=req.isSeller;
+        const order=await orderServices.getorderService(userId,isSeller);
+        return res.status(200).json({
+            status:"success",
+            data:order
+        })
+    }catch (err){
+        res.status(400).json({status:"Fail",data:err})
+    }
+}
+
+module.exports={
+    getOrders
+}
