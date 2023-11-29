8247fb913356ef04835a3c4bdb29d8a57a05b30d
Alif <79266506+S-Alif@users.noreply.github.com>
2023-11-29 18:26:30 +0600

    [UPDATE] Deleted other route files

--- Backend/src/routes/conversationRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { conversation, conversationCreate, conversationUpdate, singleConversation } = require('../controllers/conversationController');
-const authVerification = require('../middlewares/authVerification');
-
-// conversation routes
-router.post("/create-conv", authVerification, conversationCreate)
-
-module.exports = router
--- Backend/src/routes/orderRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const authVerification = require('../middlewares/authVerification')
-const {paymentIntCont}=require("../controllers/orderController")
-const router = express.Router()
-
-router.post("/create-order/:gigId",authVerification,paymentIntCont)
-//router.post("/get-order",authVerification,paymentIntCont)
-module.exports = router
